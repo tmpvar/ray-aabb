5e5075daff6cdc77105dbe13644b9a2f3c4cc7cf
Elijah Insua <tmpvar@gmail.com>
2015-01-30 13:51:39 -0800

    add basic benchmark

---
+++ bench.js
@@ -0,0 +1,32 @@
+var isect = require('./ray-aabb');
+var Suite = require('benchmark').Suite;
+
+var suite = new Suite();
+
+var boxa = [
+  [-1, -1, -1],
+  [ 1,  1,  1]
+];
+var origina = [-2, 0, 0];
+var dira = [1, 0, 0];
+var idira = [1, Infinity, Infinity]
+var neara = 0;
+var fara = 10;
+
+suite.add('raycast along x axis', function() {
+
+  if (!isect(origina, dira, idira, boxa, neara, fara)) {
+    throw new Error('fail!')
+  }
+
+})
+
+suite.on('cycle', function(event) {
+  console.log(String(event.target));
+});
+
+suite.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+});
+
+suite.run();
