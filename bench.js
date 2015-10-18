9391d7a1a9cec1b134854f59f811c4b713838c84
Elijah Insua <tmpvar@gmail.com>
2015-10-18 00:14:20 -0400

    improve benchmark

--- bench.js
+++ bench.js
@@ -8,12 +8,50 @@ var boxa = [
   [ 1,  1,  1]
 ];
 
-var origina = [-2, 0, 0];
-var dira = [1, 0, 0];
+var origin = [-2, 0, 0];
+var dir = [1, 0, 0];
 
-var ray = createRay(origina, dira);
+var ray = createRay(origin, dir);
+
+// var normal = [0, 0, 0];
+// for (var i=0; i<100000; i++) {
+//   for (var x=-1; x<=1; x++) {
+//     origin[0] = -x * 2;
+//     dir[0] = x;
+
+//     for (var y=-1; y<=1; y++) {
+//       origin[1] = -y * 2;
+//       dir[1] = y;
+
+//       for (var z=-1; z<=1; z++) {
+//         origin[2] = -z * 2;
+//         dir[2] = z;
+
+//         ray.update(origin, dir);
+//         ray.intersects(boxa, normal)
+//       }
+//     }
+//   }
+// }
+
 suite.add('raycast along x axis', function() {
-  ray.intersects(boxa);
+  for (var x=-1; x<=1; x++) {
+    origin[0] = -x * 2;
+    dir[0] = x;
+
+    for (var y=-1; y<=1; y++) {
+      origin[1] = -y * 2;
+      dir[1] = y;
+
+      for (var z=-1; z<=1; z++) {
+        origin[2] = -z * 2;
+        dir[2] = z;
+
+        ray.update(origin, dir);
+        ray.intersects(boxa)
+      }
+    }
+  }
 })
 
 suite.on('cycle', function(event) {
